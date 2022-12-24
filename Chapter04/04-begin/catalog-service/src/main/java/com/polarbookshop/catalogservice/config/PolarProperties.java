8336103ea1c077b88d7c7fd2e4d21bde7bce2b1b
yfei <feiyue1984@hotmail.com>
2022-12-24 23:26:26 +0100

    chapter end

--- Chapter04/04-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
+++ Chapter04/04-begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
@@ -1,8 +1,20 @@
 package com.polarbookshop.catalogservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConfigurationProperties(prefix = "polar")
-public record PolarProperties(String greeting) {
+@RefreshScope
+public class PolarProperties {
+
+  private String greeting;
+
+  public String getGreeting() {
+    return greeting;
+  }
+
+  public void setGreeting(String greeting) {
+    this.greeting = greeting;
+  }
 
 }
